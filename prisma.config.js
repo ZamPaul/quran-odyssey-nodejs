4473954bf5a32f14f049b32673da62df3d3d38b0
Zamiel Paul <zamielpaul2@gmail.com>
2026-05-18 02:25:10 +0500

    prisma issue fixed

--- prisma.config.js
+++ prisma.config.js
@@ -9,9 +9,7 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    // url: process.env["DATABASE_URL"],
-    // directURL: process.env["DIRECT_URL"],
-    url: process.env.DATABASE_URL,
-    directUrl: process.env.DIRECT_URL,
+    url: process.env["DATABASE_URL"],
+    shadowDatabaseUrl: process.env["DIRECT_URL"]
   },
 });
